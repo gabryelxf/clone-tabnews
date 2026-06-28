1255968275cc1cd26740154b678c967eda396477
Gabryel <gabryelxf@gmail.com>
2026-06-28 02:37:02 +0000

    adicionar strpts `lint:check` e `lint:fix`

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,9 @@
-function Home(){
-    return <h2>Hello World!</h2>
+function Home() {
+  return <h2>Hello World!</h2>;
+}
+
+function teste() {
+  console.log("teste identacao");
 }
 
 export default Home;
